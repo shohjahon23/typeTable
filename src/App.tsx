30db0c1b0a33df3ba6ca0800c6a2165b3e04e4c5
shoh_hon23 <tursunboyevshohjaxon@gmail.com>
2022-11-18 22:08:56 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Table from './components/Table';
+
+interface Cat {
+  name: string;
+  age: number;
+  gender: string;
+  color: string;
+  activityLevel?: string; // optional, same as string | undefined
+  favoriteFood?: string; // optional, same as string | undefined
+}
+
+type ColumnDefinitionType<T, K extends keyof T> = {
+  key: K;
+  header: string;
+  width?: number;
+}
+
+const data: Cat[] = [
+  {
+    name: "Mittens",
+    color: "black",
+    age: 2,
+    gender: "female",
+    activityLevel: "hight",
+    favoriteFood: "milk",
+  },
+  {
+    name: "Mons",
+    color: "grey",
+    age: 2,
+    gender: "male",
+    favoriteFood: "old socks",
+    activityLevel: "medium",
+  },
+  {
+    name: "Luna",
+    color: "black",
+    age: 2,
+    gender: "female",
+    activityLevel: "medium",
+    favoriteFood: "fish",
+  },
+  {
+    name: "Bella",
+    color: "grey",
+    age: 1,
+    gender: "female",
+    activityLevel: "high",
+    favoriteFood: "mice",
+  },
+  {
+    name: "Oliver",
+    color: "orange",
+    age: 1,
+    gender: "male",
+    activityLevel: "low",
+    favoriteFood: "fish",
+  },
+];
+
+const columns: ColumnDefinitionType<Cat, keyof Cat>[] = [
+  {
+    key: 'name',
+    header: 'Name',
+    width: 150
+  },
+  {
+    key: 'age',
+    header: 'Age in years',
+  },
+  {
+    key: 'color',
+    header: 'Color'
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table data={data} columns={columns} />
     </div>
   );
 }
